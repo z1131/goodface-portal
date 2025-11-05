d0bdb6f44cb5ab0346b2669461c0fe77cd3ba803
张紫豪 <1131220792@qq.com>
2025-11-05 10:55:19 +0800

    高可用

--- src/main/java/com/deepknow/goodface/portal/Main.java
+++ src/main/java/com/deepknow/goodface/portal/Main.java
@@ -2,10 +2,12 @@ package com.deepknow.goodface.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 
 @SpringBootApplication
 @EnableDubbo
+@EnableRedisHttpSession
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
